df7b9648a550d5736761b338e8f06fda8963cb5a
Abhinav <Abhinav.shrivastava2303@gmail.com>
2016-07-28 02:41:24 +0530

    used underscore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');	
 
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -18,14 +19,10 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoid = parseInt(req.params.id, 10);
-	var check;
-	todos.forEach(function (todo) {
-		if(todoid === todo.id) {
-			check = todo;
-		}
-	});
-	if (check) {
-		res.json(check);
+	var matchedTodo = _.findWhere(todos, {id: todoid});
+	
+	if (matchedTodo) {
+		res.json(matchedTodo);
 
 	} else {
 		res.status(404).send();
@@ -34,7 +31,12 @@ app.get('/todos/:id', function (req, res) {
 
 //POST 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextID++;
 
 	todos.push(body);

61992e7a5cdc6cb3b8e6aa82fa17afcb236737f5
Abhinav <Abhinav.shrivastava2303@gmail.com>
2016-08-10 02:02:09 +0530

    changed return for get request for 0 & 1

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.get('/users/:mobile', function (req, res) {
 	var todomobile = parseInt(req.params.mobile, 10);
 	var matcheduser = _.findWhere(users, {mobile: todomobile});
 	if (!matcheduser) {
-	res.send(false);
+	res.send(0); //0: false
 	} else {
-	res.send(true);	
+	res.send(1);	
 	}
 });
 
